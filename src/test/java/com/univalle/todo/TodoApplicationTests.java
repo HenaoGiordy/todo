4f4569edea480bc653ed6563dc05f70d430d653d
Giordy <henaogiordy@gmail.com>
2024-08-21 16:06:42 -0500

    First Commit

---
+++ src/test/java/com/univalle/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univalle.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
